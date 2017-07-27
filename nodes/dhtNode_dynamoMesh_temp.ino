3f2e4f03071598d51156162df2666ca1e2302729
Aaron Jense <faraday_induction@protonmail.com>
2017-07-26 18:44:00 -0600

    Explanation comments added

--- nodes/dhtNode_dynamoMesh_temp.ino
+++ nodes/dhtNode_dynamoMesh_temp.ino
@@ -1,17 +1,11 @@
-
-
 /*
- * The circuit:
- * LCD RS pin to digital pin 6
- * LCD Enable pin to digital pin 10
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+ * Based on RF24Mesh/examples/RF24Mesh_Example/RF24Mesh_Example.ino by tmrh20
+ *
+ * Objective: Establishes an arduino as a node in mesh network, 
+ *   	      creates a payload(nodeload) consisting of a DHT11 sensor temperature and nodeID set by user
+ *	      Each node needs a unique nodeID
+ * 
+ *
  */
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
@@ -48,42 +42,59 @@ uint32_t displayTimer = 0;
 void setup() {  
   dht.begin();
   Serial.begin(115200);
-
-  // Set the nodeID manually
+  /*
+  *  Set the nodeID manually
+  */
   mesh.setNodeID(nodeID);
-  //connect to the mesh
+  /*
+  * connect to the mesh
+  */
   Serial.println(F("Connecting to the mesh..."));
-  // configure the mesh and request an address
+  /*
+  *configure the mesh and request an address
+  */
   mesh.begin();
 }
 
 void loop() {
-
-  // similiar to network.update(), called regularly to keep network & mesh going
+  /*
+  * similiar to network.update(), called regularly to keep network & mesh going
+  */
   mesh.update();
-
+  /*
+   * Creates node payload. More variables can be added to payload, but must remain consistent with the master node receiving payload
+   */
   nodeload_t nodeload;
-  
   nodeload.temp = dht.readTemperature(true);
   nodeload.id = nodeID;
   // payload.humid = dht.readHumidity();
 
-// Increase 1000 to slow sending of payload (nodeload)
+   /* 
+    * Increase 1000 to slow sending of payload (nodeload)
+    */
 if (millis() - displayTimer > 1000) {
-  
-  displayTimer = millis();
+   displayTimer = millis();
+     /*
+      * Insures nodeload.temp is not NaN
+      */
   if (! (isnan(nodeload.temp) )) {
-    
+     /*
+      * Sends payload onto mesh network, routed to master node.
+      */
     if (! ( mesh.write(&nodeload,'M', sizeof(nodeload)) )) {
-      
+     /*
+      * If mesh connection is down, then Renew Address
+      * If the connection is up, then the send somehow failed, but may continue on
+      */
       if (! mesh.checkConnection() ) {
         Serial.println("Renewing Address");
-        mesh.renewAddress();
-        
+        mesh.renewAddress();    
       } else {
         Serial.println("Send fail, Test OK");
       }
-      
+      /*
+       * Send was a Success. Prints Status Report to Serial Monitor.
+       */
     } else {
       Serial.print("Send Ok:"); 
       Serial.print("Temp:"); Serial.print(nodeload.temp); 
@@ -92,6 +103,10 @@ if (millis() - displayTimer > 1000) {
     }
   }
 }
+  /*
+   * This checks payloads send from other nodes and routed through this node
+   * Prints status to Serial Monitor.
+   */
   while (network.available()) {
     RF24NetworkHeader header;
     network.read(header, &nodeload, sizeof(nodeload));
